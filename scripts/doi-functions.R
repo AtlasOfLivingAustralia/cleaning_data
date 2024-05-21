d47fbafc74e8032d37a3f7a8a6fa5d3ab944d70b
Dax Kellie <dax.kellie@csiro.au>
2024-05-21 18:10:59 +1000

    Implement full data doi workflow
    * Other minor edits
    * Rearrange chapters
    * Add download links to data in book chapter
    * Update column classes chapter to data doi
    * Update inspect chapter to data doi
    * Other formatting edits to column classes chapter

--- scripts/doi-functions.R
+++ scripts/doi-functions.R
@@ -3,14 +3,14 @@
 library(dplyr)
 library(tidyr)
 
-add_doi <- function(chapter, name, doi) {
+add_doi <- function(chapter, name, new_doi) {
   
   new <- tibble(chapter = {{chapter}},
                 name = {{name}},
                 doi = {{new_doi}})
   
   doi_table <- doi_table |>
-    dplyr::rows_insert(new)
+    dplyr::rows_append(new)
   
   
 }
